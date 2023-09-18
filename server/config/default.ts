afbd7e45ec46891da60843bf517df4cd33db2b80
automation2013 <1271547283@qq.com>
2023-09-18 11:06:59 +0800

    feat(nestjs): 配置服务端口

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,1 +1,5 @@
-export default {};
+export default {
+  server: {
+    port: 3000,
+  },
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const config = require('config');
+
+const port = config.get('server.port');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(port);
 }
 bootstrap();
