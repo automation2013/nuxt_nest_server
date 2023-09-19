7a58a28926bdb56e3fe23b553440536702dbaa9a
automation2013 <1271547283@qq.com>
2023-09-19 14:04:37 +0800

    feat(typeorm): 修改mysql配置

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 // 公共模块
-import { TypeormModule } from './common/database/typeorm_module';
+import { TypeormModule } from './modules/common/typeorm/typeorm_module';
 // 业务模块
 import { DemoModule } from './modules/demo/demo.module';
 @Module({
