abcdfbd68ab10068fd272b944d26eb917668f0ac
automation2013 <1271547283@qq.com>
2023-09-20 17:49:31 +0800

    feat(class-validator): 引入请求参数校验

--- server/src/modules/demo/demo.controller.ts
+++ server/src/modules/demo/demo.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { DemoService } from './demo.service';
-
+import { MethodGetDto } from './dtos/method_get.dto';
+import { MethodPostDto } from './dtos/method_post.dto';
+import { ClassValidationPipe } from '../../common/pipes/class_validation.pipe';
 @Controller('demo')
 export class DemoController {
   constructor(private readonly demoService: DemoService) {}
 
   @Get('test/get')
-  async methodGet() {
+  async methodGet(@Query(new ClassValidationPipe()) query: MethodGetDto) {
+    console.log(query);
     return await this.demoService.methodGet();
   }
 
   @Post('test/post')
-  methodPost(@Body() body) {
+  methodPost(@Body(new ClassValidationPipe()) body: MethodPostDto) {
+    console.log(body);
     return body;
   }
 }
