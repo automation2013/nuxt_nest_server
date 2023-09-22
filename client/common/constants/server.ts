e70c73a4e871f4d62448bfa3016cc0c3a6a878b4
automation2013 <1271547283@qq.com>
2023-09-22 19:58:16 +0800

    feat(nuxt): 添加get请求代理

---
+++ client/common/constants/server.ts
@@ -0,0 +1,2 @@
+// 服务端代理地址
+export const APP_SERVER_HOST = 'http://0.0.0.0:8080';
--- client/server/api/[...].ts
+++ client/server/api/[...].ts
@@ -1,7 +1,14 @@
-import { defineEventHandler, H3Event, EventHandlerRequest } from 'h3';
-export default defineEventHandler((event: H3Event<EventHandlerRequest>) => {
-  debugger;
-  return {
-    api: 'works1',
-  };
-});
+import {
+  defineEventHandler,
+  H3Event,
+  EventHandlerRequest,
+  proxyRequest,
+} from 'h3';
+import { APP_SERVER_HOST } from '../../common/constants/server';
+export default defineEventHandler(
+  async (event: H3Event<EventHandlerRequest>) => {
+    const path = event.path;
+    const url = `${APP_SERVER_HOST}${path}`;
+    return proxyRequest(event, url);
+  },
+);
