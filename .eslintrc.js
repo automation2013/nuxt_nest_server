d21711b11abb2b579e0f3ea91ead3e8d33159ad1
automation2013 <1271547283@qq.com>
2023-09-25 17:15:59 +0800

    feat(nuxtjs): 添加es-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
+    '@nuxtjs/eslint-config',
+    '@nuxtjs/eslint-config-typescript',
     'prettier', // 必须放在最后（https://github.com/prettier/eslint-config-prettier#installation）
   ],
   overrides: [
