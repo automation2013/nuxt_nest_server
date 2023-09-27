d4e33d68bfce41ff7998ccb302f3f5e896d7118a
automation2013 <1271547283@qq.com>
2023-09-27 09:28:47 +0800

    feat(nuxt): 修改代理函数

--- client/server/api/[...].post.ts
+++ client/server/api/[...].post.ts
@@ -5,10 +5,8 @@ import {
   proxyRequest,
 } from 'h3';
 import { APP_SERVER_HOST } from '../../common/constants/server';
-export default defineEventHandler(
-  async (event: H3Event<EventHandlerRequest>) => {
-    const path = event.path;
-    const url = `${APP_SERVER_HOST}${path}`;
-    return proxyRequest(event, url);
-  },
-);
+export default defineEventHandler((event: H3Event<EventHandlerRequest>) => {
+  const path = event.path;
+  const url = `${APP_SERVER_HOST}${path}`;
+  return proxyRequest(event, url);
+});
