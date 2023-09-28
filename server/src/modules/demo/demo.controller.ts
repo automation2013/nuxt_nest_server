c716cbe7d058c899a042608b932d96197f50b43c
automation2013 <1271547283@qq.com>
2023-09-28 11:21:49 +0800

    feat(nest): 添加公共模块供前后端共同使用

--- server/src/modules/demo/demo.controller.ts
+++ server/src/modules/demo/demo.controller.ts
@@ -10,9 +10,9 @@ import {
 } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Request, Response } from 'express';
+import { HttpError } from 'common-content';
 import { getCookieConfig } from '../../common/utils/cookie';
 import { IResponse } from '../../common/interfaces/respons.interface';
-import { HttpError } from '../../common/http/error';
 import { DemoService } from './demo.service';
 import { MethodGetDto } from './dtos/method_get.dto';
 import { MethodPostDto } from './dtos/method_post.dto';
