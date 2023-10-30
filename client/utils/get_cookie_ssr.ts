c20558bca258deec6fbc70df9e25363380bdeeed
automation2013 <1271547283@qq.com>
2023-10-30 09:50:01 +0800

    feat(nest): 优化服务端发送请求cookie获取

--- client/utils/get_cookie_ssr.ts
+++ client/utils/get_cookie_ssr.ts
@@ -1,18 +1,19 @@
 import { H3Event, parseCookies } from 'h3';
-import { NuxtApp } from 'nuxt/app';
+import { NuxtApp, NuxtSSRContext } from 'nuxt/app';
 
 /**
  * 服务端渲染时可获取cookie，配合 useAsyncData 方法使用，该方法回调函数的第一个参数就是 NuxtApp 类型(https://nuxt.com/docs/api/composables/use-async-data)
  * @param nuxtApp
  */
 export function getCookieSsr(nuxtApp: NuxtApp): undefined | string {
-  const event: H3Event = nuxtApp.ssrContext.event;
+  const nuxtSSRContext: NuxtSSRContext = <NuxtSSRContext>nuxtApp.ssrContext;
+  const event: H3Event = nuxtSSRContext.event;
   const cookieObj = parseCookies(event);
   const keys = Object.keys(cookieObj);
   if (!keys.length) {
     return undefined;
   }
-  const list = [];
+  const list: any[] = [];
   keys.forEach((key) => {
     const str = `${key}=${cookieObj[key]}`;
     list.push(str);
