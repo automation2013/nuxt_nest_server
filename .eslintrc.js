da59777069e7ea61a32ba2e313d86062e637f865
automation2013 <1271547283@qq.com>
2025-03-20 13:39:31 +0800

    fix(server): 删除不需要的eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
-    '@nuxtjs/eslint-config',
-    '@nuxtjs/eslint-config-typescript',
     'prettier', // 必须放在最后（https://github.com/prettier/eslint-config-prettier#installation）
   ],
   overrides: [
