013cfa9cf1a85230dee28f808e6072d3da460386
automation2013 <1271547283@qq.com>
2025-03-25 16:23:35 +0800

    feat(all): 升级elsint并配置prettier

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -13,9 +13,7 @@ export default defineNuxtConfig({
     server: true,
     client: true,
   },
-  css: [
-    '@/assets/styles/index.scss',
-  ],
+  css: ['@/assets/styles/index.scss'],
   postcss: {
     plugins: {
       // 测试autoprefixer是否生效的方法(https://github.com/postcss/autoprefixer?tab=readme-ov-file)搜索“interactive demo”
@@ -23,7 +21,7 @@ export default defineNuxtConfig({
       autoprefixer: {}, //
       'postcss-px-to-viewport': {
         unitToConvert: 'px',
-        viewportWidth: 375,// postcss-px-to-viewport-8-plugin 库的 viewportWidth 属性可以配函数，动态支持转pc和H5
+        viewportWidth: 375, // postcss-px-to-viewport-8-plugin 库的 viewportWidth 属性可以配函数，动态支持转pc和H5
         unitPrecision: 5,
         propList: ['*'],
         viewportUnit: 'vw',
@@ -40,5 +38,4 @@ export default defineNuxtConfig({
       },
     },
   },
-
 });
